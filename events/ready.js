50d8ec0fa9a4c29ce08020d0517155e8e891ac89
Peter Strick <60312421+PeterStrick@users.noreply.github.com>
2024-06-02 13:36:48 +0200

    Fix last two Issues

--- events/ready.js
+++ events/ready.js
@@ -2,15 +2,10 @@ const { ActivityType } = require("discord.js");
 const client = require("../index");
 
 client.on("ready", async () => {
-
   console.log(`${client.user.username} Is Online`);
-
 	client.user.setActivity({
 		type: ActivityType.Custom,
 		name: 'customstatus',
 		state: 'Mrrp mreow~'
 	});
-
-	//await require("../database/dbHandler")(client);
-
 });
